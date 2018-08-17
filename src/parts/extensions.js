811d0db328f4c71aa5427fb1bc04afacb878ba6e
rendfall <rendfall@gmail.com>
2018-08-18 00:13:47 +0200

    Add missing closing tags

--- src/parts/extensions.js
+++ src/parts/extensions.js
@@ -3,38 +3,34 @@ import * as xml from 'xmlbuilder';
 import { URLS } from '../constants';
 
 function buildExtensions() {
-    const extensionsDocument = xml.create('Extensions');
-
-    extensionsDocument
+    return xml.create('Extensions')
         .ele('Extension', { type: 'activeview' })
             .ele('CustomTracking')
-            .ele('Tracking', { event: 'viewable_impression' })
-                .dat(URLS.CUSTOM_TRACKING.VIEWABLE_IMPRESSION)
+                .ele('Tracking', { event: 'viewable_impression' })
+                    .dat(URLS.CUSTOM_TRACKING.VIEWABLE_IMPRESSION)
+                    .up()
+                .ele('Tracking', { event: 'abandon' })
+                    .dat(URLS.CUSTOM_TRACKING.ABANDON)
+                    .up()
+                .ele('Extension', { type: 'geo' })
+                    .ele('Country')
+                        .txt('PL')
+                        .up()
+                    .ele('Bandwidth')
+                        .txt(4)
+                        .up()
+                    .ele('BandwidthKbps')
+                        .txt(20000)
+                        .up()
+                    .ele('Extension', {
+                        type: 'pod',
+                        sequence: 1,
+                        max_fallback_attempts: 2
+                    })
+                    .up()
                 .up()
-            .ele('Tracking', { event: 'abandon' })
-                .dat(URLS.CUSTOM_TRACKING.ABANDON)
-                .up();
-
-    extensionsDocument
-        .ele('Extension', { type: 'geo' })
-            .ele('Country')
-                .txt('PL')
-                .up()
-            .ele('Bandwidth')
-                .txt(4)
-                .up()
-            .ele('BandwidthKbps')
-                .txt(20000)
-                .up();
-
-    extensionsDocument
-        .ele('Extension', { 
-            type: 'pod',
-            sequence: 1,
-            max_fallback_attempts: 2
-        });
-
-    return extensionsDocument;
+            .up()
+        .up();
 }
 
 export default {
